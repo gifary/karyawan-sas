80a56fb251e34debbcb41f587c81e5d25744b47a
gifary <gifary@gifarys-MacBook-Air.local>
2017-10-28 07:56:28 +0700

    changexy

--- src/pages/navigation/navigation.ts
+++ src/pages/navigation/navigation.ts
@@ -35,7 +35,7 @@ export class NavigationPage {
     // set our app's pages
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Permit', component: ListPermitPage },
+      { title: 'Izin/Cuti', component: ListPermitPage },
       { title: 'About', component: AboutPage },
       { title: 'Logout', component: LogoutPage },
     ];
